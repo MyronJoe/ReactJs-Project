de08d261ef4edef55639e4800e6c9ce480e9ef3d
Myron <josephonyekachi77@gmail.com>
2022-09-14 23:18:36 -0800

    React Project

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element='' />
+          <Route path='/' element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
