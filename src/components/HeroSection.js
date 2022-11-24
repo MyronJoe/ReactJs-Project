2d855c7a2ff2d9a5ef4cea78f4207d60a2361864
Myron <josephonyekachi77@gmail.com>
2022-11-23 22:07:45 -0800

    updated

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,20 +6,20 @@ import './HeroSection.css'
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src='public/video/webVideo.mp4' autoPlay loop muted />
+      <video src='public/video/webVideo.mp4' autoPlay loop muted />
 
-        <h1>MY PORTFOLIO</h1>
-        <p>GRAPHIC DESINGNER | WEB DEVELOPER - FULLSTACK | PRODUCT DESINGNER</p>
+      <h1>MY PORTFOLIO</h1>
+      <p>GRAPHIC DESINGNER | WEB DEVELOPER - FULLSTACK | PRODUCT DESINGNER</p>
 
-        <div className='btns-sec'>
-          <div className='hero-btns'>
-              <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
-          </div>
-          
-          <div className='hero-btns'>
-              <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>WATCH TRAILER <i className='fa fa-play'></i></Button>
-          </div>
+      <div className='btns-sec'>
+        <div className='hero-btns'>
+          <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
         </div>
+
+        <div className='hero-btns'>
+          <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>WATCH TRAILER <i className='fa fa-play'></i></Button>
+        </div>
+      </div>
 
     </div>
   )
